e580172268825b41898dc996e4b4c1f843d63cc1
nakabonne <ryo@nakao.dev>
2020-05-02 14:16:13 +0900

    Add from linter node to result tree

--- pkg/golangcilint/issue.go
+++ pkg/golangcilint/issue.go
@@ -6,13 +6,17 @@ import (
 
 // Issue is a wrapper of the issue model represented golangci-lint internally.
 type Issue struct {
-	issue *result.Issue
+	issue result.Issue
 }
 
 func NewIssues(issues []result.Issue) []Issue {
 	res := make([]Issue, 0, len(issues))
 	for _, i := range issues {
-		res = append(res, Issue{issue: &i})
+		res = append(res, Issue{issue: i})
 	}
 	return res
+}
+
+func (i *Issue) FromLinter() string {
+	return i.issue.FromLinter
 }
--- pkg/gui/item/results.go
+++ pkg/gui/item/results.go
@@ -27,9 +27,21 @@ func (r *Results) ShowLatest(issues []golangcilint.Issue) {
 	r.SetRoot(root).
 		SetCurrentNode(root)
 
-	r.addChildren(issues)
+	r.addChildren(root, issues)
 }
 
-func (r *Results) addChildren(issues []golangcilint.Issue) {
+func (r *Results) addChildren(node *tview.TreeNode, issues []golangcilint.Issue) {
+	linterIssues := make(map[string][]golangcilint.Issue)
+	for _, issue := range issues {
+		l := issue.FromLinter()
+		linterIssues[l] = append(linterIssues[l], issue)
+	}
 
+	for linter := range linterIssues {
+		child := tview.NewTreeNode(linter).
+			SetText("from " + linter).
+			SetSelectable(true).
+			SetColor(tcell.ColorGreen)
+		node.AddChild(child)
+	}
 }
