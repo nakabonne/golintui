89dfaa49d226c2302477313d7deff3e46ddda564
nakabonne <ryo@nakao.dev>
2020-05-04 09:45:47 +0900

    Make Linter's fields private

--- pkg/golangcilint/linter.go
+++ pkg/golangcilint/linter.go
@@ -6,15 +6,23 @@ import (
 
 // Linter represents a linter available on golangci-lint.
 type Linter struct {
-	Name    string
-	Enabled bool
+	name    string
+	enabled bool
 }
 
 // NewLinters converts LinterData represented internally into the opinionated Linter.
 func NewLinters(linters []report.LinterData) []Linter {
 	res := make([]Linter, 0, len(linters))
 	for _, l := range linters {
-		res = append(res, Linter{Name: l.Name, Enabled: l.Enabled})
+		res = append(res, Linter{name: l.Name, enabled: l.Enabled})
 	}
 	return res
+}
+
+func (l *Linter) Name() string {
+	return l.name
+}
+
+func (l *Linter) Enabled() bool {
+	return l.enabled
 }
