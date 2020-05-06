f4345d5b5c957e00c1e82e58a4cab187af3a646c
nakabonne <ryo@nakao.dev>
2020-05-06 16:31:47 +0900

    Remove info panel

--- pkg/gui/gui.go
+++ pkg/gui/gui.go
@@ -26,8 +26,8 @@ type Gui struct {
 	lintersItem     *item.Linters
 	sourceFilesItem *item.SourceFiles
 	resultsItem     *item.Results
-	infoItem        *item.Info
-	naviItem        *item.Navi
+	//infoItem        *item.Info
+	naviItem *item.Navi
 
 	runner *golangcilint.Runner
 	editor *editor.Editor
@@ -41,7 +41,6 @@ func New(logger *logrus.Entry, runner *golangcilint.Runner, command *editor.Edit
 		lintersItem:     item.NewLinters(linters),
 		sourceFilesItem: item.NewSourceFiles("."),
 		resultsItem:     item.NewResults(),
-		infoItem:        item.NewInfo(runner.GetVersion()), // TODO: Run GetVersion() concurrency
 		naviItem:        item.NewNavi(),
 		runner:          runner,
 		logger:          logger,
@@ -62,16 +61,15 @@ func (g *Gui) Run() error {
 // initGrid sets a grid based layout as a root primitive for the application.
 func (g *Gui) initGrid() {
 	grid := tview.NewGrid().
-		SetRows(1, 0, 1).
-		SetColumns(30, 40, 0, 0).
-		SetBorders(true).
-		AddItem(g.infoItem, 0, 0, 1, 2, 0, 0, false)
+		SetRows(0, 1).
+		SetColumns(30, 30, 0, 0).
+		SetBorders(true)
 
 	// Layout for screens wider than 100 cells.
-	grid.AddItem(g.lintersItem, 1, 0, 1, 1, 0, 100, true).
-		AddItem(g.sourceFilesItem, 1, 1, 1, 1, 0, 100, false).
-		AddItem(g.resultsItem, 0, 2, 2, 2, 0, 100, false).
-		AddItem(g.naviItem, 2, 0, 1, 4, 0, 0, false)
+	grid.AddItem(g.lintersItem, 0, 0, 1, 1, 0, 100, true).
+		AddItem(g.sourceFilesItem, 0, 1, 1, 1, 0, 100, false).
+		AddItem(g.resultsItem, 0, 2, 1, 2, 0, 100, false).
+		AddItem(g.naviItem, 1, 0, 1, 4, 0, 0, false)
 
 	g.naviItem.Update(g.lintersItem)
 
@@ -116,33 +114,36 @@ func (g *Gui) switchPage(prev, next string) {
 // showWarn shows the given message as a modal.
 // doneLabel param is used for the name of button to close.
 func (g *Gui) showWarn(message, doneLabel string) {
-	g.showModal(message, doneLabel, 60, 150, tcell.ColorBlack)
+	g.showModal(message, doneLabel, func() { g.switchPage(modalPageName, mainPageName) }, tcell.ColorBlack, tcell.ColorBlack, tcell.ColorWhite)
 }
 
 // showLoading shows the given message as a modal.
 // It doesn't provide any close buttons, instead it returns the function to close itself.
-func (g *Gui) showLoading(message string) func() {
-	g.showModal(message, "", 2, 100, tcell.ColorBlack)
+func (g *Gui) showLoading(message, doneLabel string) func() {
+	g.showModal(message, doneLabel, func() {}, tcell.ColorBlack, tcell.ColorWhite, tcell.ColorBlack)
 	close := func() {
 		g.switchPage(modalPageName, mainPageName)
 	}
 	return close
 }
 
-func (g *Gui) showModal(message, doneLabel string, rowWidth, colWidth int, backGroundColor tcell.Color) {
+func (g *Gui) showModal(message, doneLabel string, doneFunc func(), backGroundColor, buttonTextColor, buttonBackGroundColor tcell.Color) {
 	modal := tview.NewModal().
 		SetText(message).
-		SetBackgroundColor(backGroundColor)
+		SetBackgroundColor(backGroundColor).
+		SetButtonBackgroundColor(buttonTextColor).
+		SetButtonTextColor(buttonBackGroundColor) // NOTE: The text color and background color are reversed, which is tview's issue.
 
 	if doneLabel != "" {
 		modal.AddButtons([]string{doneLabel}).
 			SetDoneFunc(func(buttonIndex int, buttonLabel string) {
 				if buttonLabel == doneLabel {
-					g.switchPage(modalPageName, mainPageName)
+					doneFunc()
 				}
 			})
 	}
 
+	colWidth, rowWidth := 1, 1
 	grid := tview.NewGrid().
 		SetColumns(0, colWidth, 0).
 		SetRows(0, rowWidth, 0).
